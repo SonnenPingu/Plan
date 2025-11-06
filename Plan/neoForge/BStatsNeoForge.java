39bdae119b60bdee7af247a9950a6e6cdc6598df
SonnenPingu <126945262+SonnenPingu@users.noreply.github.com>
2025-11-06 10:57:37 +0100

    Add BStatsNeoForge class for metrics reporting

---
+++ Plan/neoForge/BStatsNeoForge.java
@@ -0,0 +1,57 @@
+/*
+ * This file is part of Player Analytics (Plan).
+ *
+ * Plan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License v3 as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Plan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Plan. If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.djrapitops.plan;
+
+import com.djrapitops.plan.storage.database.Database;
+import org.bstats.charts.SimplePie;
+import org.bstats.neoforge.Metrics; // Geändert
+
+import java.io.Serializable;
+
+public class BStatsNeoForge implements Runnable { // Geändert
+
+    private final Metrics metrics;
+    private final Database database;
+
+    public BStatsNeoForge(Metrics metrics, Database database) { // Geändert
+        this.metrics = metrics;
+        this.database = database;
+    }
+
+    @Override
+    public void run() {
+        registerMetrics();
+    }
+
+    public void registerMetrics() {
+        if (metrics != null) {
+            registerConfigSettingGraphs();
+        }
+    }
+
+    private void registerConfigSettingGraphs() {
+        String serverType = "NeoForge"; // Geändert
+        String databaseType = database.getType().getName();
+
+        addStringSettingPie("server_type", serverType);
+        addStringSettingPie("database_type", databaseType);
+    }
+
+    protected void addStringSettingPie(String id, Serializable setting) {
+        metrics.addCustomChart(new SimplePie(id, setting::toString));
+    }
+}
